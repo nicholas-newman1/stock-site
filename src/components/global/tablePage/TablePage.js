df8480e45a431b722a9cc83611f2886511860d72
Nicholas Newman <nicholas.newman1@hotmail.com>
2020-11-08 10:18:23 -0500

    Added titles to each page with Helmet

--- src/components/global/tablePage/TablePage.js
+++ src/components/global/tablePage/TablePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import TableNav from './TableNav';
 import Table from './Table';
 import TableOptions from './TableOptions';
@@ -6,10 +6,12 @@ import BottomNews from '../bottomNews/BottomNews';
 import { TableContext } from '../../../context/TableContext';
 import Spinner from '../Spinner';
 import '../../../css/global/tablePage/tablePage.css';
+import { Helmet } from 'react-helmet-async';
 
 const TablePage = () => {
   const path = window.location.pathname.replace('/', '');
   const { initializeTable, tableData, loading } = useContext(TableContext);
+  const [title, setTitle] = useState('');
 
   useEffect(() => {
     let sortProperty;
@@ -17,29 +19,39 @@ const TablePage = () => {
     if (path === 'cryptocurrencies') {
       sortProperty = 'marketCap';
       reverse = false;
+      setTitle('Cryptocurrency Quotes');
     } else if (path === 'forex') {
       sortProperty = 'name';
       reverse = true;
+      setTitle('Forex Quotes');
     } else if (path === 'commodities') {
       sortProperty = 'name';
       reverse = true;
+      setTitle('Commodity Quotes');
     }
     initializeTable(path, sortProperty, reverse);
     //eslint-disable-next-line
   }, [path]);
 
-  return loading ? (
-    <Spinner />
-  ) : tableData.length > 0 ? (
-    <div>
-      <h1 className='table-heading'>{path}</h1>
-      <TableNav />
-      <Table />
-      <TableOptions />
-      <BottomNews />
-    </div>
-  ) : (
-    <h2 className='table-no-data'>No Data Available</h2>
+  return (
+    <>
+      <Helmet>
+        <title>{title} | Finance App</title>
+      </Helmet>
+      {loading ? (
+        <Spinner />
+      ) : tableData.length > 0 ? (
+        <div>
+          <h1 className='table-heading'>{path}</h1>
+          <TableNav />
+          <Table />
+          <TableOptions />
+          <BottomNews />
+        </div>
+      ) : (
+        <h2 className='table-no-data'>No Data Available</h2>
+      )}
+    </>
   );
 };
 
