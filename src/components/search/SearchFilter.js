29ea24ff27a91da8b8dba5a93c10e8235cea2d99
Nicholas Newman <nicholas.newman1@hotmail.com>
2020-11-10 09:21:30 -0500

    Updated search filter styling

--- src/components/search/SearchFilter.js
+++ src/components/search/SearchFilter.js
@@ -3,6 +3,7 @@ import '../../css/search/searchFilter.css';
 
 const SearchResultsFilter = ({ setExchange }) => {
   const [checked, setChecked] = useState('');
+  const [displayFilter, setDisplayFilter] = useState(false);
 
   const exchanges = [
     'INDEX',
@@ -19,37 +20,49 @@ const SearchResultsFilter = ({ setExchange }) => {
   ];
 
   return (
-    <div className='search-filter-container'>
-      <h2 className='search-filter-header'>Filter Results</h2>
-      <form
-        className='search-filter-form'
-        onSubmit={(e) => {
-          e.preventDefault();
-          setExchange(checked);
-        }}
+    <>
+      <button
+        className='search-filter-toggle search-filter-btn'
+        onClick={() => setDisplayFilter((prev) => !prev)}
       >
-        <div className='search-filter-inputs'>
-          {exchanges.map((exchange, i) => (
-            <label className='search-filter-label' key={i}>
-              <input
-                className='search-filter-input'
-                type='checkbox'
-                value={exchange}
-                checked={checked === exchange}
-                onChange={() =>
-                  setChecked(checked === exchange ? '' : exchange)
-                }
-              />
-              {exchange}
-            </label>
-          ))}
-        </div>
+        {displayFilter ? 'Hide Filter' : 'Filter Results'}
+      </button>
+      <hr className='search-filter-hr' />
+      {displayFilter && (
+        <div className='search-filter-container'>
+          <h2 className='search-filter-header'>Filter Results</h2>
+          <form
+            className='search-filter-form'
+            onSubmit={(e) => {
+              e.preventDefault();
+              setExchange(checked);
+            }}
+          >
+            <div className='search-filter-inputs'>
+              {exchanges.map((exchange, i) => (
+                <label className='search-filter-label' key={i}>
+                  <input
+                    className='search-filter-input'
+                    type='checkbox'
+                    value={exchange}
+                    checked={checked === exchange}
+                    onChange={() =>
+                      setChecked(checked === exchange ? '' : exchange)
+                    }
+                  />
+                  {exchange}
+                </label>
+              ))}
+            </div>
 
-        <button className='search-filter-btn' type='submit'>
-          Filter
-        </button>
-      </form>
-    </div>
+            <button className='search-filter-btn submit' type='submit'>
+              Apply
+            </button>
+          </form>
+          <hr className='search-filter-hr' />
+        </div>
+      )}
+    </>
   );
 };
 
