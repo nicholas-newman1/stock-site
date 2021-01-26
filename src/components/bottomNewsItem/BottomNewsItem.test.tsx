d536571ce04f7fa972e3abf6b48f487c7d492f55
Nicholas Newman <nicholas.newman1@hotmail.com>
2021-01-26 15:44:30 -0500

    cleaned up test

--- src/components/bottomNewsItem/BottomNewsItem.test.tsx
+++ src/components/bottomNewsItem/BottomNewsItem.test.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import { cleanup, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import BottomNewsItem from './BottomNewsItem';
 
 describe('<BottomNewsItem />', () => {
-  let newsItem: NewsItem;
-  beforeEach(() => {
-    newsItem = {
-      symbol: 'TNAV',
-      publishedDate: '2020-11-03 15:44:00',
-      title:
-        'SHAREHOLDER ALERT: Levi & Korsinsky, LLP Notifies Investors of an Investigation.',
-      image: 'https://cdn.snapi.dev/images/v1/h/c/press5---l93oowtyam.jpg',
-      site: 'Newsfile Corp',
-      text:
-        'New York, New York--(Newsfile Corp. - November 3, 2020) - The following statement is being issued by Levi & Korsinsky, LLP:To: All Persons or Entities who purchased Telenav, Inc. (NASDAQ: TNAV) ("Telenav" or the "Company") stock prior to November 3, 2020.You are hereby notified that Levi & Korsinsky, LLP has commenced an investigation into the fairness of the The following statement is being issued by Levi & Korsinsky, LLP:To: All Persons or Entities who purchased Telenav, Inc. (NASDAQ: TNAV) ("Telenav" or the "Company") stock prior to November 3, 2020.You are hereby notified that Levi & Korsinsky, LLP has commenced an investigation into the fairness of the',
-      url:
-        'https://www.newsfilecorp.com/release/67422/SHAREHOLDER-ALERT-Levi-Korsinsky-LLP-Notifies-Investors-of-an-Investigation-into-the-Fairness-of-the-Sale-of-Telenav-Inc.-to-V99-Inc.',
-    } as NewsItem;
-  });
+  const newsItem = {
+    symbol: 'TNAV',
+    publishedDate: '2020-11-03 15:44:00',
+    title:
+      'SHAREHOLDER ALERT: Levi & Korsinsky, LLP Notifies Investors of an Investigation.',
+    image: 'https://cdn.snapi.dev/images/v1/h/c/press5---l93oowtyam.jpg',
+    site: 'Newsfile Corp',
+    text:
+      'New York, New York--(Newsfile Corp. - November 3, 2020) - The following statement is being issued by Levi & Korsinsky, LLP:To: All Persons or Entities who purchased Telenav, Inc. (NASDAQ: TNAV) ("Telenav" or the "Company") stock prior to November 3, 2020.You are hereby notified that Levi & Korsinsky, LLP has commenced an investigation into the fairness of the The following statement is being issued by Levi & Korsinsky, LLP:To: All Persons or Entities who purchased Telenav, Inc. (NASDAQ: TNAV) ("Telenav" or the "Company") stock prior to November 3, 2020.You are hereby notified that Levi & Korsinsky, LLP has commenced an investigation into the fairness of the',
+    url:
+      'https://www.newsfilecorp.com/release/67422/SHAREHOLDER-ALERT-Levi-Korsinsky-LLP-Notifies-Investors-of-an-Investigation-into-the-Fairness-of-the-Sale-of-Telenav-Inc.-to-V99-Inc.',
+  };
 
   it('should render without crashing', () => {
     render(<BottomNewsItem newsItem={newsItem} />);
@@ -32,9 +29,10 @@ describe('<BottomNewsItem />', () => {
   });
 
   it('should truncate title to be 120 characters or less', () => {
-    newsItem.title =
+    const newsItemCopy = { ...newsItem };
+    newsItemCopy.title =
       'SHAREHOLDER ALERT: Levi & Korsinsky, LLP Notifies Investors of an Investigation into the Fairness of the Sale of Telenav, Inc. to V99, Inc.';
-    const { getByText } = render(<BottomNewsItem newsItem={newsItem} />);
+    const { getByText } = render(<BottomNewsItem newsItem={newsItemCopy} />);
     const title = getByText(/SHAREHOLDER/);
     expect(title.textContent!.length).toBeLessThanOrEqual(120);
   });
