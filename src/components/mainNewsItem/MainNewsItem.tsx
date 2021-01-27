a9688091b3170ff7a7127a308d6dc79bc2cd708d
Nicholas Newman <nicholas.newman1@hotmail.com>
2021-01-27 05:49:31 -0500

    changed made entire MainNewsItem a link

--- src/components/mainNewsItem/MainNewsItem.tsx
+++ src/components/mainNewsItem/MainNewsItem.tsx
@@ -14,26 +14,24 @@ const MainNewsItem: React.FC<Props> = ({ data, loading }) => {
   const { site, title, text, publishedDate, url, image } = data;
 
   return (
-    <div className='main-news__item'>
+    <a href={url} className='main-news-item'>
       <div
-        className='main-news__image'
+        className='main-news-item__image'
         style={{ backgroundImage: `url(${image})` }}
       />
 
-      <div className='main-news__content'>
-        <h1 className='main-news__heading'>
-          <a href={url}>
-            {title.length > 100 ? <>{truncate(title, 100)}&hellip;</> : title}
-          </a>
+      <div className='main-news-item__content'>
+        <h1 className='main-news-item__heading'>
+          {title.length > 100 ? <>{truncate(title, 100)}&hellip;</> : title}
         </h1>
-        <p className='main-news__meta'>
+        <p className='main-news-item__meta'>
           {site} - <em>{getTimeAgoString(Date.parse(publishedDate))}</em>
         </p>
-        <p className='main-news__description'>
+        <p className='main-news-item__description'>
           {text.length > 200 ? <>{truncate(text, 200)}&hellip;</> : text}
         </p>
       </div>
-    </div>
+    </a>
   );
 };
 
