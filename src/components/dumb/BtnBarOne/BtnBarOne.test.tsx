099d1c3de9dde9610f4bd16c7361e932c7527aea
Nicholas Newman <nicholas.newman1@hotmail.com>
2021-02-02 09:01:07 -0500

    wrote tests for BtnBarOne and BtnBarTwo

---
+++ src/components/dumb/BtnBarOne/BtnBarOne.test.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import BtnBarOne from '.';
+
+describe('<BtnBarOne />', () => {
+  const btns = [
+    { text: 'home', value: '' },
+    { text: 'about' },
+    { text: 'contact', value: 'givenValue' },
+  ];
+
+  it('should render without crashing', () => {
+    render(<BtnBarOne btns={btns} setState={() => {}} />);
+  });
+
+  it('should render buttons', () => {
+    const { getByText } = render(<BtnBarOne btns={btns} setState={() => {}} />);
+    getByText('home');
+    getByText('about');
+    getByText('contact');
+  });
+
+  it('should call setState on button click', () => {
+    const fn = jest.fn();
+    const { getByText } = render(<BtnBarOne btns={btns} setState={fn} />);
+    const btn = getByText('about');
+    fireEvent.click(btn);
+    expect(fn).toBeCalled();
+  });
+
+  it('should use "text" property to update state when there is no "value" property ', () => {
+    let state;
+    const { getByText } = render(
+      <BtnBarOne btns={btns} setState={(val) => (state = val)} />
+    );
+    const btn = getByText('about');
+    fireEvent.click(btn);
+    expect(state).toBe('about');
+  });
+
+  it('should use "value" property to update state', () => {
+    let state;
+    const { getByText } = render(
+      <BtnBarOne btns={btns} setState={(val) => (state = val)} />
+    );
+    const btn = getByText('contact');
+    fireEvent.click(btn);
+    expect(state).toBe('givenValue');
+  });
+
+  it('should disable first button on mount', () => {
+    const fn = jest.fn();
+    const { getByText } = render(<BtnBarOne btns={btns} setState={fn} />);
+    const btn = getByText('home').closest('button')!;
+    expect(btn.disabled).toBe(true);
+  });
+
+  it('should disable clicked button', () => {
+    const fn = jest.fn();
+    const { getByText } = render(<BtnBarOne btns={btns} setState={fn} />);
+    const btn = getByText('about').closest('button')!;
+    fireEvent.click(btn);
+    expect(btn.disabled).toBe(true);
+  });
+
+  it('should enable all buttons except the one that was clicked last', () => {
+    const fn = jest.fn();
+    const { getByText } = render(<BtnBarOne btns={btns} setState={fn} />);
+    const btn1 = getByText('home').closest('button')!;
+    const btn2 = getByText('about').closest('button')!;
+    const btn3 = getByText('contact').closest('button')!;
+    fireEvent.click(btn2);
+    fireEvent.click(btn3);
+    expect(btn1.disabled && btn2.disabled && !btn3.disabled).toBe(false);
+  });
+});
