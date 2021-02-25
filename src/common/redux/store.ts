ff9fbd133b3a80f1624167949ed423b6d654188d
Nicholas Newman <nicholas.newman1@hotmail.com>
2021-02-25 06:54:46 -0500

    removed composeWithDevTools

--- src/common/redux/store.ts
+++ src/common/redux/store.ts
@@ -1,5 +1,5 @@
 import { createStore, compose, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk, { ThunkMiddleware } from 'redux-thunk';
 import rootReducer, { AppState } from '../../app/rootReducer';
 import { AppActions } from '../types/actionTypes';
@@ -7,8 +7,8 @@ import { AppActions } from '../types/actionTypes';
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk as ThunkMiddleware<AppState, AppActions>),
-    composeWithDevTools()
+    applyMiddleware(thunk as ThunkMiddleware<AppState, AppActions>)
+    // composeWithDevTools()
   )
 );
 
