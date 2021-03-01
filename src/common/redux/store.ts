5601149b761cbaa5fc57e723b8248bb90e1ed595
Nicholas Newman <nicholas.newman1@hotmail.com>
2021-03-01 05:46:33 -0500

    added conditionals for production/development

--- src/common/redux/store.ts
+++ src/common/redux/store.ts
@@ -1,14 +1,19 @@
 import { createStore, compose, applyMiddleware } from 'redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk, { ThunkMiddleware } from 'redux-thunk';
 import rootReducer, { AppState } from '../../app/rootReducer';
 import { AppActions } from '../types/actionTypes';
 
-const store = createStore(
+const composeEnhancers =
+  (process.env.NODE_ENV !== 'production' &&
+    typeof window !== 'undefined' &&
+    //@ts-ignore
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const store = createStore<AppState, AppActions, {}, {}>(
   rootReducer,
-  compose(
+  composeEnhancers(
     applyMiddleware(thunk as ThunkMiddleware<AppState, AppActions>)
-    // composeWithDevTools()
   )
 );
 
--- src/common/utils/api.ts
+++ src/common/utils/api.ts
@@ -16,45 +16,46 @@ export const fetchFromAPI = (
     // const res = await
 
     if (getState().realData.status) {
-      return fetch('/.netlify/functions/FMPRequest', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: btoa(
-          JSON.stringify({
-            endpoint,
-            params,
+      return process.env.NODE_ENV === 'production'
+        ? fetch('/.netlify/functions/FMPRequest', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: btoa(
+              JSON.stringify({
+                endpoint,
+                params,
+              })
+            ),
           })
-        ),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data['Error Message']) {
-            dispatch(onFailure(data['Error Message']));
-          } else if (data.hasOwnProperty('error')) {
-            dispatch(onFailure(data.error));
-          } else {
-            dispatch(onSuccess(data));
-          }
-        })
-        .catch((err) => dispatch(onFailure(err.message)));
-      // return fetch(
-      //   `https://financialmodelingprep.com/api/v3/${endpoint}?apikey=${
-      //     process.env.REACT_APP_FMP_KEY
-      //   }${params && '&' + params}`
-      // )
-      //   .then((res) => res.json())
-      //   .then((data) => {
-      //     if (data['Error Message']) {
-      //       dispatch(onFailure(data['Error Message']));
-      //     } else if (data.hasOwnProperty('error')) {
-      //       dispatch(onFailure(data.error));
-      //     } else {
-      //       dispatch(onSuccess(data));
-      //     }
-      //   })
-      //   .catch((err) => dispatch(onFailure(err.message)));
+            .then((res) => res.json())
+            .then((data) => {
+              if (data['Error Message']) {
+                dispatch(onFailure(data['Error Message']));
+              } else if (data.hasOwnProperty('error')) {
+                dispatch(onFailure(data.error));
+              } else {
+                dispatch(onSuccess(data));
+              }
+            })
+            .catch((err) => dispatch(onFailure(err.message)))
+        : fetch(
+            `https://financialmodelingprep.com/api/v3/${endpoint}?apikey=${
+              process.env.REACT_APP_FMP_KEY
+            }${params && '&' + params}`
+          )
+            .then((res) => res.json())
+            .then((data) => {
+              if (data['Error Message']) {
+                dispatch(onFailure(data['Error Message']));
+              } else if (data.hasOwnProperty('error')) {
+                dispatch(onFailure(data.error));
+              } else {
+                dispatch(onSuccess(data));
+              }
+            })
+            .catch((err) => dispatch(onFailure(err.message)));
     } else {
       return fetch(`../dummyData/${dummyDataEndpoint}.json`)
         .then((res) => res.json())
